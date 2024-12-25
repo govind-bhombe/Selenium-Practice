cb45a8f85bed98d16f8ae3e89cf762e39648311b
Govindrao Bhombe <104967470+govind-bhombe@users.noreply.github.com>
2024-12-25 12:13:13 +0530

    first commit

---
+++ src/main/java/Selenium/Prac/App.java
@@ -0,0 +1,7 @@
+package Selenium.Prac;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Selenium/Prac/xpath.java
@@ -0,0 +1,10 @@
+package Selenium.Prac;
+
+public class xpath {
+
+	public static void main(String[] args) {
+		System.out.println("Govind");
+		
+	}
+
+}
