220b6e1cca6cd6d0e2ee71cd6a71051285f5ae5a
Govindrao Bhombe <104967470+govind-bhombe@users.noreply.github.com>
2024-12-26 12:01:34 +0530

    spaces added between two numbers

--- src/test/java/Selenium/Prac/swap2No.java
+++ src/test/java/Selenium/Prac/swap2No.java
@@ -9,12 +9,12 @@ public class swap2No {
 	     Scanner sc= new Scanner(System.in);
 	     int x=sc.nextInt();
 	     int y=sc.nextInt();
-	     System.out.println("before saping:"+x+"&"+y);
+	     System.out.println("before saping:"+x+" & "+y);
 	     x=x+y;
 	     y=x-y;
 	     x=x-y;
 	     
-	     System.out.println("After swapping"+x+"&"+y);
+	     System.out.println("After swapping"+x+" & "+y);
 		
 	}
 	
